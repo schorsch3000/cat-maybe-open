dabcb17a815364326b910246b299db8a99388cdf
Dirk Heilig <heilig@adesso-mobile.de>
2019-09-12 11:45:24 +0200

    Add commandline option to ignore handler

--- cmo.php
+++ cmo.php
@@ -5,8 +5,20 @@ function logIt($msg)
     fwrite(STDERR, $msg . "\n");
 }
 
+$options = getopt("i:");
+
 $handlerConfigFile=__DIR__ . '/handlers.json';
 $handlers = json_decode(file_get_contents($handlerConfigFile), true);
+
+if(isset($options['i']))foreach((array)$options['i'] as $ignore){
+    if(!isset($handlers[$ignore])){
+        logit("Handler $ignore is not even set");
+        continue;
+    }
+    unset($handlers[$ignore]);
+}
+
+
 
 if(json_last_error()){
     logIt("Can't open handler configuration ($handlerConfigFile)");
@@ -33,6 +45,7 @@ if (!isset($handlers[$mimeType])) {
     logit("No handler set for $mimeType");
     exit;
 }
+
 $handler = $handlers[$mimeType];
 if (isset($handler['extension'])) {
     rename($tmpFile, $tmpFile . '.' . $handler['extension']);
